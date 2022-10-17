395e49e08d63ecba48fd53732869abdd8b05db2a
MartinDiCe <jamartindice@gmail.com>
2022-10-17 18:04:27 -0300

    probando crear una conexión entre el backend y el frontend

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import './App.css';
-import data from './data.json'
+import { TaskService } from './service/TaskService';
 
-function App() {
-
-  const [Tasks, setTasks] = useState(data);
-
-  console.log(Tasks)
-
-  return (
-    <div className="container">
-      Tittle
-    </div>
-  );
+//creamos el main app y extendemos del componente
+export default class App extends Component{
+  //creamos un constructor
+  constructor(){
+    //primero llamamos al constructor superior (clas padre)
+    super();
+    this.setState({
+      //aca listamos a las tareas
+      tasks: []  
+    });
+    this.state = {};
+    //llamamos a nuestro taskservice
+    this.taskService = new TaskService();
+  }
+//Metodos de ciclo de vida
+componentDidMount(){
+this.taskService.getTasks().then(data => {
+  console.log(data);
+})
 }
 
-export default App;
+//Implementaremos las libreria de la plantilla primereact (metodo render)
+render(){
+  return(
+  <h1>Hola mundo</h1>
+  );
+}
+}
+
